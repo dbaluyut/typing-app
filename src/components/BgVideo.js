5985668a7e2958045445a8e48a54d3f337032f37
dbaluyut <70111529+dbaluyut@users.noreply.github.com>
2024-09-24 21:02:50 -0800

    change vid links

--- src/components/BgVideo.js
+++ src/components/BgVideo.js
@@ -2,15 +2,11 @@ import { useState, useEffect } from 'react'
 
 function BgVideo({ index }) {
   const videoLinks = [
-    'https://thumbs.gfycat.com/BreakableGlaringImpala-mobile.mp4',
-    'https://thumbs.gfycat.com/InsignificantPlainBlobfish-mobile.mp4',
-    'https://thumbs.gfycat.com/AchingUnsightlyHarrierhawk-mobile.mp4',
+    'https://tenor.com/view/lofi-girl-lofi-study-gif-22173420',
   ]
 
   const els = [
-    <source src='https://thumbs.gfycat.com/BreakableGlaringImpala-mobile.mp4'></source>,
-    <source src='https://thumbs.gfycat.com/InsignificantPlainBlobfish-mobile.mp'></source>,
-    <source src='https://thumbs.gfycat.com/AchingUnsightlyHarrierhawk-mobile.mp4'></source>,
+    <source src='https://tenor.com/view/lofi-girl-lofi-study-gif-22173420'></source>,
   ]
 
   const [ci, setCI] = useState(1)
