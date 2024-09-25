4d1761f5a3546e47be5efa5fd22eccddb52ba61f
Derrique B <deebz@MacBook-Pro.local>
2024-09-24 21:39:52 -0800

    temp bg vid fix

--- src/components/BgVideo.js
+++ src/components/BgVideo.js
@@ -1,38 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function BgVideo({ index }) {
   const videoLinks = [
-    'https://tenor.com/view/lofi-girl-lofi-study-gif-22173420',
-  ]
+    "https://tenor.com/view/lofi-girl-lofi-study-gif-22173420",
+  ];
 
   const els = [
-    <source src='https://tenor.com/view/lofi-girl-lofi-study-gif-22173420'></source>,
-  ]
+    <source src="https://tenor.com/view/lofi-girl-lofi-study-gif-22173420"></source>,
+  ];
 
-  const [ci, setCI] = useState(1)
+  const [ci, setCI] = useState(1);
   //   const [currentBG, setCurrentBG] = useState(
   //     videoLinks[currentIndex]
   //   )
 
-  let currentBG = videoLinks[ci]
+  let currentBG = videoLinks[ci];
 
   function changeBG() {
     for (let i = 1; i < els.length; i++) {
       setTimeout(function () {
-        setCI((ci) => ci + 1)
-        console.log(ci)
-      }, 3000)
+        setCI((ci) => ci + 1);
+        console.log(ci);
+      }, 3000);
     }
   }
 
   useEffect(() => {
     // setTimeout(changeBG, 10000)
-  }, [ci])
+  }, [ci]);
   return (
-    <video playsInline autoPlay muted loop id='bgVideoLoop'>
-      <source src={currentBG}></source>
-    </video>
-  )
+    <div id="bgVideoLoop">
+      <div
+        style={{
+          width: "100%",
+          height: 0,
+          paddingBottom: "100%",
+          position: "relative",
+        }}
+      >
+        <iframe
+          src="https://giphy.com/embed/JGMaGy5beukJ96I5Xw"
+          width="100%"
+          height="100%"
+          style={{position: 'absolute'}}
+          frameBorder="0"
+          class="giphy-embed"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <p>
+        <a href="https://giphy.com/gifs/savesoil-merry-christmas-lofi-girl-lofigirl-JGMaGy5beukJ96I5Xw">
+          via GIPHY
+        </a>
+      </p>
+    </div>
+  );
 }
 
-export default BgVideo
+export default BgVideo;
